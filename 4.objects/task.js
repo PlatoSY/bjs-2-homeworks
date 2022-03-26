5d5a9baced2395860c60b5aa15458a50791b042b
PlatoSY <sergeplato@gmail.com>
2022-03-26 13:28:29 +0300

    Homework-JS 4.objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,39 @@
 function Student(name, gender, age) {
-    // Ваш код
-
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 }
+//const student1 = new Student();
+//const student2 = new Student();
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
-}
+  this.subject = subjectName;
+};
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark];
+  } else {
+    this.marks.push(mark);
+  }
+};
+Student.prototype.addMarks = function (mark1, mark2, mark3, ...rest) {
+  if (this.marks === undefined) {
+    this.marks = [mark1, mark2, mark3, ...rest];
+  } else {
+    this.marks.push(mark1, mark2, mark3, ...rest);
+  }
+};
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  for (let studentMark of this.marks) {
+    sum += studentMark;
+  }
+  avg = sum / this.marks.length;
+  return avg;
+};
 
-// ваш код для остальных методов
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+  this.excluded = reason;
+};
