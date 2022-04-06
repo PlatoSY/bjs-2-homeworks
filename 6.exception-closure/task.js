fdf7c5f2d02c306e3a246a9fdfc69d3121c8d1a3
PlatoSY <sergeplato@gmail.com>
2022-04-06 12:02:10 +0300

    6.exception-closure modified task.js 1.2

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -3,12 +3,11 @@
 function parseCount(number) {
   console.log(number);
   let parsed = Number.parseInt(number);
-  // console.log(parsed);
-
   if (isNaN(parsed)) {
-    const nonLiquid = new Error(`Невалидное значение`);
-    throw nonLiquid;
-  } else return parsed;
+    throw new Error(`Невалидное значение`);
+  } else {
+    return parsed;
+  }
 }
 function validateCount(value) {
   try {
@@ -25,37 +24,34 @@ class Triangle {
     this.b = b;
     this.c = c;
     if (a + b < c || a + c < b || b + c < a) {
-      const wrongTriangle = new Error(
-        `Треугольник с такими сторонами не существует`
-      );
-      throw wrongTriangle;
+      throw new Error(`Треугольник с такими сторонами не существует`);
     }
   }
   getPerimeter() {
     return this.a + this.b + this.c;
   }
   getArea() {
-    let p = (1 / 2) * this.getPerimeter();
-    let triangleSquare = Math.sqrt(
-      p * (p - this.a) * (p - this.b) * (p - this.c)
+    const p = (1 / 2) * this.getPerimeter();
+    const triangleSquare = Number(
+      Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c)).toFixed(3)
     );
-    let triangleSquareNumber = Number(triangleSquare.toFixed(3));
-    return triangleSquareNumber;
+    return triangleSquare;
+    // let triangleSquareNumber = Number(triangleSquare.toFixed(3));
+    // return triangleSquareNumber;
   }
+}
 
-  //совершенно не понимаю логику , которая должна быть внизу. Как только я пытаюсь писать эту функцию, рушатся все проверки тестов , которые верные были до этого . Ошибка вверху где-то? Новый объект треугольникак это же через new делается? Как его вернуть с двумя методами?
-
-  //   getTriangle (a,b,c) {
-  //       let triangleObject = new Triangle;
-  //       try {
-  //           return triangleObject ;
-  //       } catch {
-  //           getArea() {
-  //               return `Ошибка! Треугольник не существует`
-  //           },
-  //           getPerimeter() {
-  //               return `Ошибка! Треугольник не существует`
-  //           }
-  //       }
-  //   }
+function getTriangle(a, b, c) {
+  try {
+    return new Triangle(a, b, c);
+  } catch {
+    return {
+      getArea() {
+        return `Ошибка! Треугольник не существует`;
+      },
+      getPerimeter() {
+        return `Ошибка! Треугольник не существует`;
+      },
+    };
+  }
 }
